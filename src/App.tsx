50f67b6d4ab135bc47bd9253d607565a9b8ea3aa
Hossam Mourad <ihos4m@gmail.com>
2022-11-06 22:20:27 -0500

    better styling

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ const App = () => {
       return (
         <span
           key={x}
-          className="border p-4"
+          className="border p-4 rounded"
           onDragOver={e => e.preventDefault()}
           onDrop={(e) => onDrop(e, x)}
         >
           <p className="font-semibold" onDrop={(e) => onDrop(e, x)}>{x}</p>
           {cars[x].length > 0 &&
             <span className="flex gap-2 mt-2" onDrop={(e) => onDrop(e, x)}>
-              {cars[x].map(y => <span key={y} onDrop={(e) => onDrop(e, x)} className="bg-blue-100 px-4 py-2">{y}</span>)}
+              {cars[x].map(y => <span key={y} onDrop={(e) => onDrop(e, x)} className="bg-slate-100 px-4 py-2 rounded">{y}</span>)}
             </span>
           }
         </span>
@@ -27,7 +27,7 @@ const App = () => {
 
   const renderPeople = () => {
     return people.map(x => {
-      return <span key={x} className="border px-4 py-2" draggable onDragStart={() => onDragStart(x)}>{x}</span>;
+      return <span key={x} className="border px-4 py-2 rounded" draggable onDragStart={() => onDragStart(x)}>{x}</span>;
     });
   };
 
@@ -66,7 +66,7 @@ const App = () => {
   return (
     <main className="p-4">
       <section>
-        <h1 className="font-semibold text-lg mb-2">
+        <h1 className="font-semibold text-lg mb-2 px-4 py-3 rounded bg-blue-100">
           What, Where, When
         </h1>
         <input
@@ -82,29 +82,29 @@ const App = () => {
         <input
           type="text"
           placeholder="Gathering time..."
-          className="border border-gray-100 px-3 py-2 mb-2 w-full rounded-md placeholder:text-sm"
+          className="border border-gray-100 px-3 py-2 w-full rounded-md placeholder:text-sm"
         />
       </section>
 
-      <section className="my-4">
-        <h1 className="font-semibold text-lg mb-2">
+      <section className="my-7">
+        <h1 className="flex items-center font-semibold text-lg mb-2 px-4 py-3 rounded bg-blue-100">
           People
-          <span className="text-xs font-normal ml-2 text-gray-400">🙋‍♂️ Who is coming?</span>
+          <span className="text-xs font-normal ml-auto text-gray-400">🙋‍♂️ Who is coming?</span>
         </h1>
         <div className="flex gap-2">
           {renderPeople()}
-          <button className="border px-4 py-2 text-green-600 font-semibold cursor-pointer" onClick={addPeopleOnClick}>Add +</button>
+          <button className="border px-4 py-2 text-blue-500 font-semibold cursor-pointer" onClick={addPeopleOnClick}>Add +</button>
         </div>
       </section>
 
       <section>
-        <h1 className="font-semibold text-lg mb-2">
+        <h1 className="flex items-center font-semibold text-lg mb-2 px-4 py-3 rounded bg-blue-100">
           Carpooling
-          <span className="text-xs font-normal ml-2 text-gray-400">🚘 Who is driving?</span>
+          <span className="text-xs font-normal ml-auto text-gray-400">🚘 Who is driving?</span>
         </h1>
         <div className="flex flex-col gap-2">
           {renderCars()}
-          <span className="border px-4 py-2 text-green-600 font-semibold cursor-pointer" onClick={addCarOnClick}>Add +</span>
+          <span className="border px-4 py-2 text-blue-500 font-semibold cursor-pointer" onClick={addCarOnClick}>Add +</span>
         </div>
       </section>
     </main>
